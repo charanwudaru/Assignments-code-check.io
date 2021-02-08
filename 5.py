75486fa696f1ceca5721208c39c5b58351df83b2
charan sai reddy .w <72861534+charanwudaru@users.noreply.github.com>
2021-02-08 21:33:12 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -3,10 +3,10 @@ y = 0
 z = 0 
 
 if x==y==z:
-   print("x = y + z ")
-elif x==y+z:
+   print("None found")
+elif y==x+z:
    print("y = x + z")
 elif z==x+y:
    print("z = x + y")
-elif y==x+z:
-   print("None found")
+elif x==y+z:
+   print("x = y + z "
