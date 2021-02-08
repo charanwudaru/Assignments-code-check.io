1221dc4fb82c8bf19df0a0a0f97fd3a7debe029b
wudaru charan sai reddy <charan@gitam.in>
2021-02-08 15:47:33 +0530

    good

---
+++ code check.py
@@ -0,0 +1,11 @@
+
+
+last = ['a1', 'h1', 'a8', 'h8']
+margin = ['b1','c1','d1','e1','f1','g1','a2','a3','a4','a5','a6','a7','b8','c8','d8','e8','f8','g8','h2','h3','h4','h5','h6','h7']
+
+if(inputStr in corner):
+   print("Corner")
+elif(inputStr in border):
+   print("Border")
+else:
+   print("Inside")
